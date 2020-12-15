aec8a2ff5ccaa87111db0797d41bfe64d8ed7db7
Lucas Sartore <lucas.sartore@gmail.com>
2020-12-15 15:32:07 +0800

    Refactor day 15

--- 2020/15/15.py
+++ 2020/15/15.py
@@ -1,10 +1,9 @@
-from collections import Counter
 
 def lastindex(numbers, n):
     return len(numbers) - 1 - numbers[::-1].index(n)
 
 def using_list(numbers, stop):
-    said = numbers.copy()
+    said = numbers[:]
     for i in range(len(numbers), stop):
         previous = said[i-1]
         if previous in said[:-1]:
@@ -18,33 +17,21 @@ def using_dict(numbers, stop):
     last_said = None
 
     for current_index in range(len(numbers), stop):
-        if last_said is None:
-            inserting = 0
-        else:
-            inserting = current_index-last_said-1
-
-        if inserting in said:
-            last_said = said[inserting]
-        else:
-            last_said = None
-
+        inserting = 0 if last_said is None else current_index - last_said - 1
+        last_said = said[inserting] if inserting in said else None
         said[inserting] = current_index
 
         # Pretty progress bar
         if current_index % 3000000 == 0:
             length = 20
-            progress = int(current_index / stop * length)
-            line = '[' + '-' * progress + ' ' * (length - progress) + ']'
-            print(line)
+            print(f'[{"-"*int(current_index / stop * length):<{length}s}]')
 
     return inserting
 
 def main():
-    starting_numbers = '12,20,0,6,1,17,7'
-    numbers = [int(x) for x in starting_numbers.split(',')]
-
+    numbers = [12,20,0,6,1,17,7]
     print("Part 1:", using_list(numbers, 2020)) # Too slow for part 2
     print("Part 1:", using_dict(numbers, 2020))
-    print("Part 2:", using_dict(numbers, 30000000))
+    print("Part 2:", using_dict(numbers, 10000000))
 
 main()
